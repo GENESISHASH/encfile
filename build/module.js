7098f4bff204ccce49a1f596c8d55f8a6491b407
tosadvisor <taky@taky.com>
2017-06-02 21:09:14 -0400

    compile

--- build/module.js
+++ build/module.js
@@ -76,7 +76,7 @@
       _ref = ['info', 'warn', 'error'];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         x = _ref[_i];
-        if (!process.env.SILENCE) {
+        if (!process.env.ENCFILE_SILENCE) {
           _this[x] = winston[x];
         }
       }
@@ -85,30 +85,124 @@
   })(this)();
 
   module.exports = encfile = {
-    std: ((function(_this) {
-      return function() {
-        var help;
-        help = (function() {
-          log("Usage: ./ <command> <options>\n  Command: encrypt file (e):\n    ./ e --infile <filename> --outfile <filename> --key <secret>\n  Command: decrypt file (d):\n    ./ d --infile <filename> --outfile <filename> --key <secret> [--meta]");
-          return exit(0);
+    std: (function() {
+      var a, cmd, e, err, help, i, item, r, required, x, ___iced_passed_deferral, __iced_deferrals, __iced_k, _i, _j, _len, _len1;
+      __iced_k = __iced_k_noop;
+      ___iced_passed_deferral = iced.findDeferral(arguments);
+      help = ((function(_this) {
+        return function(code) {
+          log("Usage: ./ <command> <options>\n  Command: encrypt file (e):\n    ./ e --infile <filename> --outfile <filename> --key <secret>\n  Command: decrypt file (d):\n    ./ d --infile <filename> --outfile <filename> --key <secret>");
+          return exit(code != null ? code : 0);
+        };
+      })(this));
+      if (_.arg('help')) {
+        help();
+      }
+      a = process.argv;
+      i = 0;
+      cmd = null;
+      for (_i = 0, _len = a.length; _i < _len; _i++) {
+        item = a[_i];
+        if (item === 'e' || item === 'encrypt') {
+          cmd = 'e';
+          break;
+        }
+        if (item === 'd' || item === 'decrypt') {
+          cmd = 'd';
+          break;
+        }
+        ++i;
+      }
+      if (!cmd) {
+        l.error('No valid command specified');
+        help(1);
+      }
+      required = ['infile', 'outfile', 'key'];
+      for (_j = 0, _len1 = required.length; _j < _len1; _j++) {
+        x = required[_j];
+        if (!_.arg(x)) {
+          err = "`--" + x + "` required";
+          l.error(err);
+          exit(1);
+        }
+      }
+      (function(_this) {
+        return (function(__iced_k) {
+          if (cmd === 'e') {
+            (function(__iced_k) {
+              __iced_deferrals = new iced.Deferrals(__iced_k, {
+                parent: ___iced_passed_deferral,
+                filename: "/Users/douglaslauer/www/encfile/src/module.iced"
+              });
+              _this.encrypt(_.arg('infile'), _.arg('outfile'), _.arg('key'), __iced_deferrals.defer({
+                assign_fn: (function() {
+                  return function() {
+                    e = arguments[0];
+                    return r = arguments[1];
+                  };
+                })(),
+                lineno: 73
+              }));
+              __iced_deferrals._fulfill();
+            })(function() {
+              if (e) {
+                l.err(e);
+                exit(1);
+              }
+              return __iced_k(exit(0));
+            });
+          } else {
+            return __iced_k();
+          }
         });
-        if (_.arg('help')) {
-          return help();
-        }
-      };
-    })(this)),
-    encrypt: ((function(_this) {
-      return function(infile, outfile, key, cb) {
-        var data, e, json, start, ___iced_passed_deferral, __iced_deferrals, __iced_k;
-        __iced_k = __iced_k_noop;
-        ___iced_passed_deferral = iced.findDeferral(arguments);
-        start = new Date;
-        infile = _.resolve(infile);
-        outfile = _.resolve(outfile);
-        if (!_.exists(infile)) {
-          return cb(new Error("" + infile + " does not exist"));
-        }
-        (function(__iced_k) {
+      })(this)((function(_this) {
+        return function() {
+          (function(__iced_k) {
+            if (cmd === 'd') {
+              (function(__iced_k) {
+                __iced_deferrals = new iced.Deferrals(__iced_k, {
+                  parent: ___iced_passed_deferral,
+                  filename: "/Users/douglaslauer/www/encfile/src/module.iced"
+                });
+                _this.decrypt(_.arg('infile'), _.arg('outfile'), _.arg('key'), __iced_deferrals.defer({
+                  assign_fn: (function() {
+                    return function() {
+                      e = arguments[0];
+                      return r = arguments[1];
+                    };
+                  })(),
+                  lineno: 80
+                }));
+                __iced_deferrals._fulfill();
+              })(function() {
+                if (e) {
+                  l.err(e);
+                  exit(1);
+                }
+                l.info("File metadata", JSON.stringify(r));
+                return __iced_k(exit(0));
+              });
+            } else {
+              return __iced_k();
+            }
+          })(function() {
+            return help(0);
+          });
+        };
+      })(this));
+    }),
+    encrypt: (function(infile, outfile, key, cb) {
+      var data, e, json, start, ___iced_passed_deferral, __iced_deferrals, __iced_k;
+      __iced_k = __iced_k_noop;
+      ___iced_passed_deferral = iced.findDeferral(arguments);
+      start = new Date;
+      infile = _.resolve(infile);
+      outfile = _.resolve(outfile);
+      if (!_.exists(infile)) {
+        return cb(new Error("" + infile + " does not exist"));
+      }
+      (function(_this) {
+        return (function(__iced_k) {
           __iced_deferrals = new iced.Deferrals(__iced_k, {
             parent: ___iced_passed_deferral,
             filename: "/Users/douglaslauer/www/encfile/src/module.iced"
@@ -120,10 +214,12 @@
                 return data = arguments[1];
               };
             })(),
-            lineno: 51
+            lineno: 99
           }));
           __iced_deferrals._fulfill();
-        })(function() {
+        });
+      })(this)((function(_this) {
+        return function() {
           if (e) {
             l.error(e);
             return cb(e);
@@ -152,7 +248,7 @@
                   return json = arguments[1];
                 };
               })(),
-              lineno: 72
+              lineno: 120
             }));
             __iced_deferrals._fulfill();
           })(function() {
@@ -172,7 +268,7 @@
                     return e = arguments[0];
                   };
                 })(),
-                lineno: 80
+                lineno: 128
               }));
               __iced_deferrals._fulfill();
             })(function() {
@@ -184,19 +280,19 @@
               return cb(null, outfile);
             });
           });
-        });
-      };
-    })(this)),
-    decrypt: ((function(_this) {
-      return function(infile, outfile, key, cb) {
-        var e, err, json, start, ___iced_passed_deferral, __iced_deferrals, __iced_k;
-        __iced_k = __iced_k_noop;
-        ___iced_passed_deferral = iced.findDeferral(arguments);
-        if (!_.exists(infile)) {
-          return cb(new Error("" + infile + " does not exist"));
-        }
-        start = new Date;
-        (function(__iced_k) {
+        };
+      })(this));
+    }),
+    decrypt: (function(infile, outfile, key, cb) {
+      var e, err, json, start, ___iced_passed_deferral, __iced_deferrals, __iced_k;
+      __iced_k = __iced_k_noop;
+      ___iced_passed_deferral = iced.findDeferral(arguments);
+      if (!_.exists(infile)) {
+        return cb(new Error("" + infile + " does not exist"));
+      }
+      start = new Date;
+      (function(_this) {
+        return (function(__iced_k) {
           __iced_deferrals = new iced.Deferrals(__iced_k, {
             parent: ___iced_passed_deferral,
             filename: "/Users/douglaslauer/www/encfile/src/module.iced"
@@ -208,10 +304,12 @@
                 return json = arguments[1];
               };
             })(),
-            lineno: 96
+            lineno: 144
           }));
           __iced_deferrals._fulfill();
-        })(function() {
+        });
+      })(this)((function(_this) {
+        return function() {
           if (e) {
             l.error(e);
             return cb(new Error(e));
@@ -229,7 +327,7 @@
                   return json = arguments[1];
                 };
               })(),
-              lineno: 102
+              lineno: 150
             }));
             __iced_deferrals._fulfill();
           })(function() {
@@ -259,7 +357,7 @@
                     return e = arguments[0];
                   };
                 })(),
-                lineno: 119
+                lineno: 167
               }));
               __iced_deferrals._fulfill();
             })(function() {
@@ -268,96 +366,99 @@
                 return cb(new Error(e));
               }
               l.info("File written to disk in " + (new Date - start) + "ms");
-              return cb(null, outfile);
+              return cb(null, json._);
             });
           });
-        });
-      };
-    })(this))
+        };
+      })(this));
+    })
   };
 
   if (!module.parent) {
-    arr = [file_orig = __dirname + '/../test/image.png', file_encd = __dirname + '/../test/image.enc', file_copy = __dirname + '/../test/image-copy.png', file_fail = __dirname + '/../test/image-fail.png'];
-    ((function(_this) {
-      return function() {
-        var f, x, _i, _len, _results;
-        x = _.clone(arr);
-        x.shift();
-        if (_.exists(f)) {
-          _results = [];
-          for (_i = 0, _len = x.length; _i < _len; _i++) {
-            f = x[_i];
-            _results.push(rm(f));
-          }
-          return _results;
-        }
-      };
-    })(this))();
     (function(_this) {
       return (function(__iced_k) {
-        __iced_deferrals = new iced.Deferrals(__iced_k, {
-          filename: "/Users/douglaslauer/www/encfile/src/module.iced"
-        });
-        encfile.encrypt(file_orig, file_encd, 'secret', __iced_deferrals.defer({
-          assign_fn: (function() {
-            return function() {
-              e = arguments[0];
-              return r = arguments[1];
-            };
-          })(),
-          lineno: 145
-        }));
-        __iced_deferrals._fulfill();
-      });
-    })(this)((function(_this) {
-      return function() {
-        if (e) {
-          throw e;
-        }
-        (function(__iced_k) {
-          __iced_deferrals = new iced.Deferrals(__iced_k, {
-            filename: "/Users/douglaslauer/www/encfile/src/module.iced"
-          });
-          encfile.decrypt(file_encd, file_copy, 'secret', __iced_deferrals.defer({
-            assign_fn: (function() {
-              return function() {
-                e = arguments[0];
-                return r = arguments[1];
-              };
-            })(),
-            lineno: 148
-          }));
-          __iced_deferrals._fulfill();
-        })(function() {
-          if (e) {
-            throw e;
-          }
+        if (_.arg('test')) {
+          arr = [file_orig = __dirname + '/../test/image.png', file_encd = __dirname + '/../test/image.enc', file_copy = __dirname + '/../test/image-copy.png', file_fail = __dirname + '/../test/image-fail.png'];
+          (function() {
+            var f, x, _i, _len, _results;
+            x = _.clone(arr);
+            x.shift();
+            if (_.exists(f)) {
+              _results = [];
+              for (_i = 0, _len = x.length; _i < _len; _i++) {
+                f = x[_i];
+                _results.push(rm(f));
+              }
+              return _results;
+            }
+          })();
           (function(__iced_k) {
             __iced_deferrals = new iced.Deferrals(__iced_k, {
               filename: "/Users/douglaslauer/www/encfile/src/module.iced"
             });
-            encfile.decrypt(file_encd, file_fail, 'different', __iced_deferrals.defer({
+            encfile.encrypt(file_orig, file_encd, 'secret', __iced_deferrals.defer({
               assign_fn: (function() {
                 return function() {
                   e = arguments[0];
                   return r = arguments[1];
                 };
               })(),
-              lineno: 152
+              lineno: 193
             }));
             __iced_deferrals._fulfill();
           })(function() {
-            if (!e) {
-              throw new Error('This should have failed');
+            if (e) {
+              throw e;
             }
-            log("Finished");
-            return __iced_k(exit(0));
+            (function(__iced_k) {
+              __iced_deferrals = new iced.Deferrals(__iced_k, {
+                filename: "/Users/douglaslauer/www/encfile/src/module.iced"
+              });
+              encfile.decrypt(file_encd, file_copy, 'secret', __iced_deferrals.defer({
+                assign_fn: (function() {
+                  return function() {
+                    e = arguments[0];
+                    return r = arguments[1];
+                  };
+                })(),
+                lineno: 196
+              }));
+              __iced_deferrals._fulfill();
+            })(function() {
+              if (e) {
+                throw e;
+              }
+              (function(__iced_k) {
+                __iced_deferrals = new iced.Deferrals(__iced_k, {
+                  filename: "/Users/douglaslauer/www/encfile/src/module.iced"
+                });
+                encfile.decrypt(file_encd, file_fail, 'different', __iced_deferrals.defer({
+                  assign_fn: (function() {
+                    return function() {
+                      e = arguments[0];
+                      return r = arguments[1];
+                    };
+                  })(),
+                  lineno: 200
+                }));
+                __iced_deferrals._fulfill();
+              })(function() {
+                if (!e) {
+                  throw new Error('This should have failed');
+                }
+                log("Finished");
+                return __iced_k(exit(0));
+              });
+            });
           });
-        });
-      };
-    })(this));
+        } else {
+          return __iced_k(encfile.std());
+        }
+      });
+    })(this)(__iced_k);
   } else {
-    __iced_k();
+        process.env.ENCFILE_SILENCE = 1;
+    __iced_k();;
   }
 
 }).call(this);
